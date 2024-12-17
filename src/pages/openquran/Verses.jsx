a7e55780a9441d7f17a52f7bd65212060abb0ab9
hakan.basturknewinn.com <forxynet@gmail.com>
2024-12-17 16:38:11 +0300

    updated css

--- src/pages/openquran/Verses.jsx
+++ src/pages/openquran/Verses.jsx
@@ -5,6 +5,7 @@ export default function Verses() {
   const surahs = useLoaderData();
   const [selectedSureId, setSelectedSureId] = useState(null);
   const [selectedSureName, setSelectedSureName] = useState(null);
+  const [selectedSureVerseCount, setSelectedSureVerseCount] = useState(null);
   const [verses, setVerses] = useState(null);
 
   useEffect(() => {
@@ -17,6 +18,7 @@ export default function Verses() {
         const data = await response.json();
         setVerses(data);
         setSelectedSureName(data.data.name);
+        setSelectedSureVerseCount(data.data.verse_count);
         console.log(data.data);
       }
 
@@ -44,30 +46,50 @@ export default function Verses() {
         <div>
           {verses && verses.data.verses.map((ayet) => (
             <>
-              <div className='verses-details-1'>
-                <div className='surah-name'>
-                   <p>{`${selectedSureId}. sûre ${selectedSureName}`}</p>
-                </div>
-                <p>{`${ayet.id}. ayet`}</p>
-                <p>{`${ayet.page}. sayfa ${ayet.juz_number}. cüz`}</p>
+              <div className='wrapper'>
 
-                <div className='verses-details'>
-                  <p>{ayet.verse}</p>
+                <div className='header'>
+                  <p>{selectedSureName} Sûresi</p>
                 </div>
-                <div className="verses-details-1">
-                  <p>{ayet.transcription}</p>
-                </div>               
-                <div className='verses-details-1'>
-                  <p>{ayet.translation.text}</p>
+
+                <div className='main'>
+                  <p className="verses-details">{ayet.verse}</p>
+                  <p className="verses-details-1">{ayet.transcription}</p>
+                  <p className="verses-details-1">{ayet.translation.text}</p>
                 </div>
+                {/* <div className="aside aside-2">{`${selectedSureId}. Sûre ${selectedSureVerseCount}`}</div>
+                <div className="aside aside-1">{`${ayet.verse_number}. Ayet`}</div> */}
+                <div className="footer">
+                  <div className="info-main ">
+                    <div className="info-container">                     
+                      <p className="info-item">Sure No</p>
+                      <p className="info-item">Sure Ayet No</p>
+                      <p className="info-item">Ayet Sayısı</p>
+                      <p className="info-item">Sayfa No</p>
+                      <p className="info-item">Cüz No</p>
+                      <p className="info-item">Ayet No</p>
+                    </div>
+                    <div className="info-container">                     
+                      <p className="info-item">{selectedSureId}</p>
+                      <p className="info-item">{ayet.verse_number}</p>
+                      <p className="info-item">{selectedSureVerseCount}</p>
+                      <p className="info-item">{ayet.page}</p>
+                      <p className="info-item">{ayet.juz_number}</p>
+                      <p className="info-item">{ayet.id}</p>
+                    </div>
+                  </div>
+                </div>
+                {/* {`${ayet.page}. Sayfa ${ayet.juz_number}. Cüz ${ayet.id}. Ayet`}</div> */}
+
 
                 {
                   ayet.translation.foodnotes &&
                   ayet.translation.foodnotes.map(notes => (
-                      <p key={notes.id}>{ notes.text}</p>
+                    <p key={notes.id}>{notes.text}</p>
                   ))
                 }
                 {/* {ayet.translation !== null && console.log(ayet.translation)} */}
+
 
               </div>
             </>
