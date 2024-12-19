dbcab8b309b823c4894e4be82c814d14a847646b
hakan.basturknewinn.com <forxynet@gmail.com>
2024-12-19 15:46:37 +0300

    updated search verses component

--- src/components/SearchTable.jsx
+++ src/components/SearchTable.jsx
@@ -7,7 +7,7 @@ export default function SearchTable(surahs) {
   const [loading, setLoading] = useState(true);
   const [verses, setVerses] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
-
+  const [count, setCount] = useState(0);
   const myArray = [];
 
   useEffect(() => {
@@ -52,8 +52,9 @@ export default function SearchTable(surahs) {
       filteredArray.push(
         data.filter(
           (item) =>
-            item.verse.includes(searchTerm.toLowerCase()) ||
-            item.translation.text.includes(searchTerm.toLowerCase())
+            item.verse.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            item.transcription.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            item.translation.text.toLowerCase().includes(searchTerm.toLowerCase())
         )
       )
     );
@@ -63,12 +64,13 @@ export default function SearchTable(surahs) {
     );
     filteredResultData.push(result);
 
-    //console.log("result:" + result.length);
+    setCount(result.length);
+    console.log("result:" + result);
     //console.log("filteredResultData:" + filteredResultData.length);
 
     setFilteredData(...filteredResultData);
 
-    //console.log("filteredData" + filteredData.length);
+    console.log(filteredData);
 
     /** resul console */
     // filteredResultData.map(item => (
@@ -84,68 +86,74 @@ export default function SearchTable(surahs) {
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="seach by arabic or text"
-        value={searchTerm}
-        onChange={(e) => {
-          setSearchTerm(e.target.value);
-          setFilteredData(null);
-        }}
-        className="mb-4 p-2 border border-gray-300 rounded"
-      />
-      &nbsp;
-      <button onClick={handleFilteredData}>Search</button>
+      <p>
+        <input
+          type="text"
+          placeholder="seach by arabic or text"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+            setFilteredData(null);
+            setCount(null)
+          }}
+          className="mb-4 p-2 border border-gray-300 rounded"
+        />
+        &nbsp;
+        <button onClick={handleFilteredData}>Search</button>
+      </p>
+      <p>
+        Bulunan kayıt saysı: {count}
+      </p>
+      <div className="container">
+        <div>
+          {loading && <div>Loading</div>}
+          {!loading && filteredData !== null &&
+            filteredData.map((data) =>
+              data.map((item) => (
+                <>
+                  <div className="main" key={item.id}>
+                    <p className="verses-details">{`${item.verse} (${item.surah_id}:${item.verse_number})`}</p>
+                    <p className="verses-details-1">{`(${item.surah_id}:${item.verse_number}) ${item.transcription}`}</p>
+                    <p className="verses-details-1">{`(${item.surah_id}:${item.verse_number}) ${item.translation.text}`}</p>
+                    <div
+                      style={{
+                        display:
+                          item.translation.footnotes !== null ? "block" : "none",
+                      }}
+                    >
+                      <hr />
+                      <div className="verses-footnotes">
+                        {
+                          item.translation.footnotes !== null &&
+                          item.translation.footnotes.map(footnotes => (
+                            <p key={footnotes.id}>{`[${footnotes.number}] ${footnotes.text}`}</p>
+                          ))
+                        }
+                      </div>
+                    </div>
 
-      {loading && <div>Loading</div>}
-      {!loading && filteredData !== null &&
-        filteredData.map((item) =>
-          item.map((item) => (
-            <>
-              <div className=''>
-                <div className="main" key={item.id}>
-                  <p className="verses-details">{`${item.verse} (${item.surah_id}:${item.verse_number})`}</p>
-                  <p className="verses-details-1">{`(${item.surah_id}:${item.verse_number}) ${item.transcription}`}</p>
-                  <p className="verses-details-1">{`(${item.surah_id}:${item.verse_number}) ${item.translation.text}`}</p>
-
-                  <div
-                    style={{
-                      display:
-                        item.translation.footnotes !== null ? "block" : "none",
-                    }}
-                  >
-                    <hr />
-                    <div className="verses-footnotes">
-                      {
-                        item.translation.footnotes !== null &&
-                        item.translation.footnotes.map(footnotes => (
-                          <p key={footnotes.id}>{`[${footnotes.number}] ${footnotes.text}`}</p>
-                        ))
-                      }
+                    <div className="footer">
+                      <hr />
+                      <div className="info-main ">
+                        <div className="info-container">
+                          <p className="info-item">Sure No</p>
+                          <p className="info-item">Sayfa</p>
+                          <p className="info-item">Cüz</p>
+                        </div>
+                        <div className="info-container">
+                          <p className="info-item">{item.surah_id}</p>
+                          <p className="info-item">{item.page}</p>
+                          <p className="info-item">{item.juz_number}</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
 
-                  <div className="footer">
-                    <hr />
-                    <div className="info-main ">
-                      <div className="info-container">
-                        <p className="info-item">Sure No</p>
-                        <p className="info-item">Sayfa</p>
-                        <p className="info-item">Cüz</p>
-                      </div>
-                      <div className="info-container">
-                        <p className="info-item">{item.surah_id}</p>
-                        <p className="info-item">{item.page}</p>
-                        <p className="info-item">{item.juz_number}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </>
-          ))
-        )}
-
+                </>
+              ))
+            )}
+        </div>
+      </div>
     </div>
   );
 }
