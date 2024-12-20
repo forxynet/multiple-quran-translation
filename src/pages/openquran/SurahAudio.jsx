8d9acb9d962c2e9c225cf92be6f86f566f0df743
hakan.basturknewinn.com <forxynet@gmail.com>
2024-12-21 02:19:26 +0300

    updated surah play screen

--- src/pages/openquran/SurahAudio.jsx
+++ src/pages/openquran/SurahAudio.jsx
@@ -1,10 +1,15 @@
 import { useLoaderData } from "react-router"
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import { useState } from 'react';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 
-import AudioPlayer from 'material-ui-audio-player';
+//import AudioPlayer from 'material-ui-audio-player';
 
 export async function loader() {
   const response = await fetch('https://api.acikkuran.com/surahs');
@@ -18,129 +23,47 @@ export async function loader() {
 export default function SurahAudio() {
   const surahs = useLoaderData();
 
+  const [sureId, setSureId] = useState(null)
 
-  // const muiTheme = createMuiTheme({});
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setSureId(event.target.value);
+  };
 
-  // const useStyles = makeStyles((theme) => {
-  //   return {
-  //     root: {
-  //       [theme.breakpoints.down('sm')]: {
-  //         width: '100%',
-  //       },
-  //     },
-  //     loopIcon: {
-  //       color: '#3f51b5',
-  //       '&.selected': {
-  //         color: '#0921a9',
-  //       },
-  //       '&:hover': {
-  //         color: '#7986cb',
-  //       },
-  //       [theme.breakpoints.down('sm')]: {
-  //         display: 'none',
-  //       },
-  //     },
-  //     playIcon: {
-  //       color: '#f50057',
-  //       '&:hover': {
-  //         color: '#ff4081',
-  //       },
-  //     },
-  //     replayIcon: {
-  //       color: '#e6e600',
-  //     },
-  //     pauseIcon: {
-  //       color: '#0099ff',
-  //     },
-  //     volumeIcon: {
-  //       color: 'rgba(0, 0, 0, 0.54)',
-  //     },
-  //     volumeSlider: {
-  //       color: 'black',
-  //     },
-  //     progressTime: {
-  //       color: 'rgba(0, 0, 0, 0.54)',
-  //     },
-  //     mainSlider: {
-  //       color: '#3f51b5',
-  //       '& .MuiSlider-rail': {
-  //         color: '#7986cb',
-  //       },
-  //       '& .MuiSlider-track': {
-  //         color: '#3f51b5',
-  //       },
-  //       '& .MuiSlider-thumb': {
-  //         color: '#303f9f',
-  //       },
-  //     },
-  //   };
-  // });
+  console.log(surahs.data);
+
+
+
 
   return (
     <div className='chapters'>
-      {surahs.data.map((sure) => (
-        <>
 
-          <Card sx={{ display: 'flex', marginTop: 5 }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <CardContent sx={{ flex: '1 0 auto' }}>
-                <Typography component="div" variant="h5">
-                  {sure.name}
-                </Typography>
-                <Typography
-                  variant="subtitle1"
-                  component="div"
-                  sx={{ color: 'text.secondary' }}
-                >
-                  {sure.name_original}
-                </Typography>
-              </CardContent>
-              {/* <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-                <IconButton aria-label="previous">
-                  {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-                </IconButton>
-                <IconButton aria-label="play/pause">
-                  <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-                </IconButton>
-                <IconButton aria-label="next">
-                  {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-                </IconButton>
-              </Box> */}
-
-
-
-              <AudioPlayer
-                width="500px"
-
-                src={sure.audio.mp3}
-                loop={true}
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        {surahs.data.map((sure) => (
+          <>
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                {sure.verse_count}
+              </ListItemAvatar>
+              <ListItemText value={sure.id}
+                primary={sure.name_original}
+                secondary={
+                  <>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      sx={{ color: 'text.primary', display: 'inline' }}
+                    >
+                      {sure.name}
+                    </Typography>
+                  </>
+                }
               />
-            </Box>
-          </Card>
-
-          {/* <div className='chapters-details-1' >
-            <p>{sure.id}</p>
-            <p>{sure.name}</p>
-            <p>{sure.name_en}</p>
-            <p>{sure.name_original}</p>
-            <p>{sure.verse_count}</p>
-
-            <div className='surahs-details' key={sure.id}>
-              <div id="container-player">
-                <div id="playerDiv">
-                  <AudioPlayer lang="TR" url={sure.audio.mp3} name={sure.name} />
-                </div>
-              </div>
-
-              <div id="container-player">
-                <div id="playerDiv">
-                  <AudioPlayer lang="EN" url={sure.audio.mp3_en} name={sure.name_en} />
-                </div>
-              </div>
-            </div>
-          </div> */}
-        </>
-      ))}
+            </ListItem >
+            <Divider variant="inset" component="li" />
+          </>
+        ))}
+      </List>
     </div>
   )
 }
