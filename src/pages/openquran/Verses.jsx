439597b8808568930cf9b3e993d6cb3db24998f3
hakan.basturknewinn.com <forxynet@gmail.com>
2024-12-20 12:34:25 +0300

    updated verses

--- src/pages/openquran/Verses.jsx
+++ src/pages/openquran/Verses.jsx
@@ -100,7 +100,7 @@ export default function Verses() {
                     <div className="info-container">
                       <p className="info-item">{`Sûre ${ayet.surah_id}`}</p>
                       <p className="info-item">{`Ayet ${ayet.verse_number}`}</p>
-                      <p className="info">{`Sayfa  ${ayet.page}:${ayet.id}. Ayet`}</p>
+                      <p className="info">{`Sayfa  ${ayet.page}:${ayet.id}`}</p>
                       <p className="info-item">{`Cüz   ${ayet.juz_number}`}</p>
                     </div>
                   </div>
