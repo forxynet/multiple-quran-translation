825cf483b3246ed93344a6b4d55ccb0a0585b0cb
hakan.basturknewinn.com <forxynet@gmail.com>
2024-12-23 00:41:50 +0300

    updated surahs play

--- src/components/AccordionPlay.jsx
+++ src/components/AccordionPlay.jsx
@@ -1,30 +1,38 @@
+import { useState } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
+
 import Buttons from './Buttons';
 
 export default function AccordionPlay(surahs) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    console.log(panel)
+    setExpanded(isExpanded ? panel : false);
+  };
 
   return (
     <>
       {
         surahs && surahs.surahs.data.map(sure => (
           <>
-            <Accordion>
+            <Accordion expanded={expanded === `panel-${sure.id}`} onChange={handleChange(`panel-${sure.id}`)}>
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header">
+                aria-controls={`panel${sure.id}-content`}
+                id={`panel-${sure.id}`}>
                 {`${sure.name} (${sure.name_original})`}
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  <Buttons name={`(TR) ${sure.name} (${sure.name_original})`} url={sure.audio.mp3} id={sure.id} />
+                  <Buttons name={`(TR) ${sure.name} (${sure.verse_count})`} url={sure.audio.mp3} id={sure.id} />
                 </Typography>
                 <Typography>
-                  <Buttons name={`(EN) ${sure.name} (${sure.name_original})`} url={sure.audio.mp3_en} id={sure.id} />
+                  <Buttons name={`(EN) ${sure.name} (${sure.verse_count})`} url={sure.audio.mp3_en} id={sure.id} />
                 </Typography>
               </AccordionDetails>
             </Accordion>
